8c43eed403b28fcef851011e0fac6e7db115e6dd
avagyansamvel <avagyansamvel07@gmail.com>
2021-11-27 15:51:39 +0400

    Add Mocks for models

---
+++ Shop/Data/Mocks/MockCar.cs
@@ -0,0 +1,32 @@
+﻿using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shop.Data.Mocks
+{
+    public class MockCar : ICarRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategory();
+
+        public IEnumerable<Car> GetAll()
+        {
+            return new List<Car>
+            {
+                new Car { Name = "BMW", Category = _categoryRepository.GetAll().Last() },
+                new Car { Name = "Tesla", Category = _categoryRepository.GetAll().First() }
+            };
+        }
+
+        public Car GetCar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Car> GetFavouriteCars()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Shop/Data/Mocks/MockCategory.cs
@@ -0,0 +1,18 @@
+﻿using Shop.Data.Interfaces;
+using Shop.Data.Models;
+using System.Collections.Generic;
+
+namespace Shop.Data.Mocks
+{
+    public class MockCategory : ICategoryRepository
+    {
+        public IEnumerable<Category> GetAll()
+        {
+            return new List<Category>
+            {
+                new Category{ Name = "ElectroCars", Description = "Modern type of cars"},
+                new Category{ Name = "ClassicCars", Description = "Cars with fuel etc."}
+            };
+        }
+    }
+}
