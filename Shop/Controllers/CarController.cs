fb466f7003a510a9dc0bfa40cf6f517a426c2f56
avagyansamvel <avagyansamvel07@gmail.com>
2021-11-27 23:33:12 +0400

    Add CarController

---
+++ Shop/Controllers/CarController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data.Interfaces;
+
+namespace Shop.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly ICarRepository _carRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CarController(ICarRepository carRepository, ICategoryRepository categoryRepository)
+        {
+            _carRepository = carRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var cars = _carRepository.GetAll();
+            return View(cars);
+        }
+    }
+}
--- Shop/Startup.cs
+++ Shop/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Data.Interfaces;
+using Shop.Data.Mocks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +18,8 @@ namespace Shop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICarRepository, MockCar>();
+            services.AddTransient<ICategoryRepository, MockCategory>();
             services.AddMvc();
         }
 
