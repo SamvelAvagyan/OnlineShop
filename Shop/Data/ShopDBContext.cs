e60b9599dc148ba5df07cf0b4ae97cf6f3ca06dc
avagyansamvel <avagyansamvel07@gmail.com>
2021-11-28 11:47:45 +0400

    Add EntityFramework, DBContext

---
+++ Shop/Data/ShopDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data
+{
+    public class ShopDBContext : DbContext
+    {
+        public ShopDBContext(DbContextOptions<ShopDBContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Car> Car { get; set; }
+        public DbSet<Category> Category { get; set; }
+    }
+}
